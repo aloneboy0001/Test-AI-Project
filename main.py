652bb8bbf6b226cacde562321d7e933715c60294
aloneboy0001 <130388990+aloneboy0001@users.noreply.github.com>
2025-05-02 18:25:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from SimplerLLM.language.llm import LLM, LLMProvider
 
-llm_instance = LLM.create(provider=LLMProvider.OPENAI, model_name="gpt-4o")
+llm_instance = LLM.create(provider=LLMProvider.GEMINI, model_name="gemini-1.5-flash")
 
-generated_text = llm_instance.generate_response(prompt="")
+generated_text = llm_instance.generate_response(prompt="generate a sentence of 5 words")
 
 print(generated_text)
